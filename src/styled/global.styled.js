60e59f5f171cdc3da75e344a833edd1e74599c91
gjlacerda <guilherme.lcd@gmail.com>
2017-09-28 22:26:49 -0300

    Created global style file

---
+++ src/styled/global.styled.js
@@ -0,0 +1,11 @@
+import {injectGlobal} from 'styled-components';
+
+injectGlobal`
+    @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700');
+    
+    html, 
+    body,
+    #root {
+        height: 100%;    
+    }
+`;
