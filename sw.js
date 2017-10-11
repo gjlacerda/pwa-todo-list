784797d59eadd182bb4d86a2ce2b139991167e07
gjlacerda <guilherme.lcd@gmail.com>
2017-10-10 23:55:21 -0300

    Changed sw to cache files correctly

---
+++ sw.js
@@ -0,0 +1,30 @@
+const version = '0.6.3';
+
+self.addEventListener('install', e => {
+    console.log('install');
+    const timeStamp = Date.now();
+    e.waitUntil(
+        caches.open('airhorner').then(cache => {
+            return cache.addAll([
+                `/`,
+                `/index.html?timestamp=${timeStamp}`,
+                `/src/js/bundle.js?timestamp=${timeStamp}`,
+            ])
+                        .then(() => self.skipWaiting());
+        })
+    )
+});
+
+self.addEventListener('activate',  event => {
+    console.log('activate');
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+    console.log('fetch');
+    event.respondWith(
+        caches.match(event.request, {ignoreSearch:true}).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
