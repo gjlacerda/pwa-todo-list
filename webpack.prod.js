510241b592165e58898837f6e9a25f3bcab23db5
gjlacerda <guilherme.lcd@gmail.com>
2017-10-17 21:16:01 -0200

    Build to prod

---
+++ webpack.prod.js
@@ -0,0 +1,33 @@
+const path    = require('path');
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: [
+        './src/index.js'
+    ],
+    output: {
+        path: path.resolve('dist'),
+        filename: 'src/js/bundle.js',
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: ['babel-loader?presets[]=react,presets[]=es2015']
+            }
+        ]
+    },
+    resolve: {
+        modules: [
+            'src',
+            'node_modules'
+        ]
+    },
+    plugins: [
+        new CopyWebpackPlugin([
+            { from: './src/icons/', to: 'src/icons/' }
+        ]),
+    ]
+};
